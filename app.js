ace420e1f4e5ee724c268e4345fa9fce2d3e60ef
TZY2 <tzhanyu@gmail.com>
2019-06-05 16:06:21 +0800

    .

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+
+const mainRoute = require('./routes/main');
+
+app.use('/', mainRoute);
+
+
+
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main' // Specify default template views/layout/main.handlebar 
+}));
+app.set('view engine', 'handlebars');
+
+// Creates static folder for publicly accessible HTML, CSS and Javascript files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/*	
+* Creates a unknown port 5000 for express server since we don't want our app to clash with well known
+* ports such as 80 or 8080.
+* */
+const port = 5000;
+
+// Starts the server and listen to port 5000
+app. listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
