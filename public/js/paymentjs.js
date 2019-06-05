33c9880356f24b796f5cfd717041788b131e9fa8
TZY2 <tzhanyu@gmail.com>
2019-06-05 23:18:16 +0800

    asd

--- public/js/paymentjs.js
+++ public/js/paymentjs.js
@@ -2,7 +2,7 @@ function changeText() {
     var element = document.getElementById('paylink');
     if (element.innerHTML === 'Pay') element.innerHTML = 'Paid';
     else{
-        
+        element.innerHTML = 'Paid';
     }
 }
 
