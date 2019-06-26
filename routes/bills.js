765f0852eaf6ca866526c94c98ee612b8a841734
TZY2 <tzhanyu@gmail.com>
2019-06-26 17:48:44 +0800

    asd

--- routes/bills.js
+++ routes/bills.js
@@ -46,5 +46,52 @@ router.post('/addbills',(req, res) => {
 
 });
 
+// Shows edit video page
+router.get('/edit/:id', (req, res) => {
+    Bills.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then((bills) => {
+
+        if (req.user.id === bills.userId) {
+            checkOptions(bills);
+            // call views/video/editVideo.handlebar to render the edit video page
+            res.render('bills/editbills', {
+                bills // passes video object to handlebar
+            });
+        } else {
+            // Video does not belong to the current user
+            alertMessage(res, 'danger', 'Unauthorized Access.', 'fas fa-exclamation-circle', true);
+            req.logout();
+            res.redirect('/');
+        }
+    }).catch(err => console.log(err)); // To catch no video ID
+});
+
+// Save edited video
+router.put('/saveEditedBills/:id',(req, res) => {
+    let title = req.body.title;
+    let billCost = req.body.billCost.slice(0, 50);
+    let dateRelease = moment(req.body.dateRelease, 'DD/MM/YYYY');
+    let userId = req.user.id;
+    var billID = req.params.id;
+    // Retrieves edited values from req.body
+    Bills.update({
+        // Set variables here to save to the videos table
+        title,
+        billCost,
+        dateRelease
+    }, {
+            where: {
+                id: billID
+            }
+        }).then(() => {
+            // After saving, redirect to router.get(/listVideos...) to retrieve all updated
+            // videos
+            res.redirect('/bills/payment');
+        }).catch(err => console.log(err));
+});
+
 
 module.exports = router;
