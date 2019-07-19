2f7a776d9eb793d6f9daa3f04b7ee22b94a0705c
TZY2 <tzhanyu@gmail.com>
2019-07-19 22:12:22 +0800

    asd

--- routes/bills.js
+++ routes/bills.js
@@ -55,8 +55,12 @@ router.get('/edit/:id', (req, res) => {
             id: req.params.id
         }
     }).then((bills) => {
-
-        if (req.user.id === bills.userId) {
+        res.render('bills/editbills',{
+            bills
+        });
+    })
+});
+        /*if (req.user.id === bills.userId) {
             checkOptions(bills);
             // call views/video/editVideo.handlebar to render the edit video page
             res.render('bills/editbills', {
@@ -68,8 +72,7 @@ router.get('/edit/:id', (req, res) => {
             req.logout();
             res.redirect('/');
         }
-    }).catch(err => console.log(err)); // To catch no bills id
-});
+    }).catch(err => console.log(err)); // To catch no bills id*/
 
 // Save edited video
 router.put('/saveEditedBills/:id',(req, res) => {
@@ -95,15 +98,14 @@ router.put('/saveEditedBills/:id',(req, res) => {
 });
 
 router.get('/delete/:id',(req, res) => {
-    var billsId = req.params.id;
     Bills.findOne({
         where: {
-            id: billsId
+            id: req.params.id
         }
     }).then((bills) => {
         console.log("billsIDToDelete.userId : " + bills.userId);
         console.log("req.user.id : " + req.user.id);
-        if (video.userId === req.user.id) {
+        if (bills.userId === req.user.id) {
             Bills.destroy({
                 where: {
                     id: billsId
